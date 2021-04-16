b8f3d7838063bec9ea1b9116a96b25847591d7d5
cls <zaintariq.alphasquad@gmail.com>
2021-04-16 11:46:25 +0500

    Minor changes.

--- components/Task/task.js
+++ components/Task/task.js
@@ -104,8 +104,11 @@ export default function Task() {
         <div className="pb-5 text-center">
           <button className={`${styles.mainDiv} focus-within:outline-none px-4 py-2 rounded-full shadow-md  text-white`} onClick={showData}>Click To Console Data</button>
         </div>
+        <div className="text-center">
+          <p className="text-2xl"> Text area should be displayed here After Entering. </p>
+        </div>
         <div>
-        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
                 <table class="min-w-full leading-normal">
                     <thead>
