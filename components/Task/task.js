dd2089e5163472055c06c75e13657b70f40cadb4
cls <zaintariq.alphasquad@gmail.com>
2021-04-16 11:42:54 +0500

    table added.

--- components/Task/task.js
+++ components/Task/task.js
@@ -104,6 +104,52 @@ export default function Task() {
         <div className="pb-5 text-center">
           <button className={`${styles.mainDiv} focus-within:outline-none px-4 py-2 rounded-full shadow-md  text-white`} onClick={showData}>Click To Console Data</button>
         </div>
+        <div>
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table class="min-w-full leading-normal">
+                    <thead>
+                        <tr>
+                            <th
+                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                ID
+                            </th>
+                            <th
+                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                Data.
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <tr> */}
+                          {
+                            data.map((result,key)=>{
+                              return (
+                                <tr>
+                                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm w-2/5">
+                                    <div class="flex items-center">
+
+                                      <div class="ml-3">
+                                          <p class="text-gray-900 whitespace-no-wrap">
+                                              {result.id}
+                                          </p>
+                                      </div>
+                                  </div>
+                              </td>
+                              <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                  <p class="text-gray-900 whitespace-no-wrap text-center">{result.name}</p>
+                              </td>
+                             </tr> 
+                              )  
+                            })
+                          }
+                        {/* </tr> */}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        </div>
+
      </div>
     </div>
   )
